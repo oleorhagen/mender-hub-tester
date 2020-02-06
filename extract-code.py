a1fedab0b93b7317c92320ae47037ae2d596795c
Ole Petter <ole.orhagen@northern.tech>
2020-02-06 15:39:28 +0100

    Moved the code rendering to a parser subclass
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- extract-code.py
+++ extract-code.py
@@ -1,15 +1,8 @@
-# from mistune.mistune import mistune
-# import mistune.mistune as m
+
+import re
 
 from mistune import mistune
 
-import pprint
-import tempfile
-import re
-
-# from mistune.renderers import BaseRenderer
-# from mistune.mistune import renderers
-from mistune.mistune.renderers import BaseRenderer
 
 """
 This tool will parse <file> to extract enclosed ```bash ``` code blocks from markdown,
@@ -28,105 +21,78 @@ add it to a shell script, and execute it (with -e -x set).
 # Create a code renderer
 class DocumentationCodeRenderer(mistune.AstRenderer):
     NAME = 'code'
-    IS_TREE = False
+    IS_TREE = True
 
-    # TODO -- Make the parser handle AUTOMATION HTML comments, like code blocks.
-    def block_code(self, code, info=None):
-        if info:
-            lang = info.strip().split(None, 1)[0]
-            lang = escape_html(lang)
-            print("Lang: {}".format(lang))
-        return "lang: {}"
+    IGNORE_NEXT_CODE_BLOCK = False
+
+    def __init__(self, output_file=None):
+        if output_file is not None:
+            self.output_file = output_file
 
 
-renderer = DocumentationCodeRenderer()
+    # Hidden documentation automation commands
+    AUTOMATION_EXECUTE_REGEXP = "<!---\s*AUTOMATION: execute=`(.*)`\s*-->"
+    AUTOMATION_IGNORE_REGEXP = "<!---\s*AUTOMATION: ignore=`(.*)`\s*-->"
+    AUTOMATION_TEST_REGEXP = "<!---\s*AUTOMATION: test=`(.*)`\s*-->"
 
-# Generate AST tokens instead of html
-markdown = mistune.create_markdown(renderer="ast")
+    def block_html(self, children):
+        # Check if it is an automation comment.
+        #
+        # Type: execute
+        #
+        me = re.match(self.AUTOMATION_EXECUTE_REGEXP, children)
+        if me:
+            self.output_file.write("\n# Added as an automation comment\n")
+            self.output_file.write(str(me.group(1))+"\n\n")
+            return None
+        #
+        # Type: Ignore
+        #
+        me = re.match(self.AUTOMATION_IGNORE_REGEXP, children)
+        if me:
+            self.IGNORE_NEXT_CODE_BLOCK = True
+            self.output_file.write("\n# Ignoring the next code block...\n")
+            return None
+        #
+        # Type: test
+        #
+        me = re.match(self.AUTOMATION_TEST_REGEXP, children)
+        if me:
+            self.output_file.write("\n# Test code block\n")
+            self.output_file.write(str(me.group(1))+"\n\n")
+            return None
+        return None
 
+
+    def block_code(self, children, info=None):
+        """Only render a block of code if it is bash marked, and it is not ignored"""
+        if info == "bash":
+          if self.IGNORE_NEXT_CODE_BLOCK:
+              self.IGNORE_NEXT_CODE_BLOCK = False
+              self.output_file.write("# Ignoring...\n")
+              for line in children.split("\n"):
+                  self.output_file.write("# {}\n".format(line))
+              return None
+          self.output_file.write(children)
+        return None
 
 code_blocks = []
 
-AUTOMATION_EXECUTE_REGEXP = "<!---\s*AUTOMATION: execute=`(.*)`\s*-->"
-AUTOMATION_IGNORE_REGEXP = "<!---\s*AUTOMATION: ignore=`(.*)`\s*-->"
-AUTOMATION_TEST_REGEXP = "<!---\s*AUTOMATION: test=`(.*)`\s*-->"
+with open("article.md", mode="r") as af:
 
-# Traverse the AST...
-def DFS(node):
-    print(node)
-    if node["type"] == "block_code":
-        code_blocks.append(node)
-    # Parse the special cases of AUTOMATION HTML comments
-    # Ideally this should be done in the parser, but POC man.
-    if node["type"] == "block_html":
-        me = re.match(AUTOMATION_EXECUTE_REGEXP, node["text"])
-        if me:
-            code_blocks.append(
-                {
-                    "type": "execute",
-                    "text": str(me.group(1)+"\n"),
-                    "info": "automation"
-                })
-        me = re.match(AUTOMATION_IGNORE_REGEXP, node["text"])
-        if me:
-            code_blocks.append(
-                {
-                    "type": "ignore",
-                    "text": str(me.group(1)+"\n"),
-                    "info": "automation"
-                })
-        me = re.match(AUTOMATION_TEST_REGEXP, node["text"])
-        if me:
-            code_blocks.append(
-                {
-                    "type": "test",
-                    "text": str(me.group(1)+"\n"),
-                    "info": "automation"
-                })
-    if "children" in node:
-      for child in node["children"]:
-          DFS(child)
-
-with open("article.md") as af:
     data = af.read()
-    a = markdown(data)
 
-    for root in a:
-        DFS(root)
+    try:
+      output_file = open("test-script", mode="w")
+    except Exception as e:
+        print("Error opening file: {}".format(e))
 
-    code_to_test = []
+    renderer = DocumentationCodeRenderer(output_file)
 
-    print("Extracted code blocks:")
-    print(len(code_blocks))
-    for block in code_blocks:
-        # Strip the sections which should not be executed
-        print(block["type"])
-        if block["info"] != None:
-            code_to_test.append(block)
+    # Generate AST tokens instead of html
+    markdown = mistune.create_markdown(renderer=renderer)
 
-    print("Code to test:")
-    print(code_to_test)
+    markdown(data)
 
-    # Create a script out of the commands
-    name = ""
-    with open("test-script", "w") as tf:
-        name = tf.name
-        print(tf)
-        tf.write("#! /bin/bash\n")
-        tf.write("set -e -x\n")
-        command_iter = iter(code_to_test)
-        for command in command_iter:
-            print ("Running command: ")
-            print(command)
-            if command["type"] == "ignore":
-                tf.write("# Ignoring next command:\n# reason: {}".format(command["text"]))
-                nc = next(command_iter)
-                tf.write("# {}\n".format(nc["text"]))
-                continue
-            if command["type"] == "test":
-                tf.write("# Documentation test command\n")
-            if command["type"] == "execute":
-                tf.write("# The next execution step is hidden in the documentation\n")
-            tf.write(str(command["text"]))
+    output_file.close()
 
-    print("Tempfile name: {}".format(name))

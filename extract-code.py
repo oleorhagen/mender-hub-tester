01c956cca69d8dc3f01d9e6321ead66442ff122d
Ole Petter <ole.orhagen@northern.tech>
2020-02-11 13:40:04 +0100

    Formatted the file with Black
    Changelog: None
    
    Signed-off-by: Ole Petter <ole.orhagen@northern.tech>

--- extract-code.py
+++ extract-code.py
@@ -1,24 +1,90 @@
 #! /bin/python3
+"""
+Usage:
+
+extract-code.py infile.md outfile.bash
+
+This tool will parse 'infile.md' to extract the code enclosed ```bash ``` code blocks from
+ markdown, and add it to a 'outfile.md' given on the command line.
+
+Extra instructions are taken from HTML comments in the markdown, whereas three comment types
+are supported. Descriptions of which can be found below:
+
+* Usually when following documentation, some human interaction is required. This can be
+  achieved by adding:
+      <!--- AUTOMATION: execute=`cmd` -->
+  blocks to the markdown file.
+
+* Sometimes, some instructions enclosed in ```bash ``` may not be required and skipped.
+  This can be achieved by adding:
+      <!--- AUTOMATION: ignore=`reason` -->
+
+* Finally, testing that the instructions executed correctly can be tested using:
+      <!--- AUTOMATION: test=`cmd` -->
+
+
+Example:
+
+The output script will be given a header like:
+
+~~~~~~~~~~~~~~~~~~~~
+#! /bin/bash
+set -e -x
+~~~~~~~~~~~~~~~~~~~~
+
+Then any bash command will simply be translated into a bash command in the output script.
+
+Ignored bash code-blocks like:
+
+~~~~~~~~~~~~~~~~~~~~
+<!--- AUTOMATION ignore=`ignore next cause foobar` -->
+```bash
+echo "I'm useless"
+```
+~~~~~~~~~~~~~~~~~~~~
+
+Will result in:
+~~~~~~~~~~~~~~~~~~~~
+# Ignoring the next code block...
+# Ignoring...
+# echo "I'm useless"
+#
+~~~~~~~~~~~~~~~~~~~~
+
+And automation commands like:
+
+~~~~~~~~~~~~~~~~~~~~
+<!--- AUTOMATION execute=`echo "Run me"` -->
+~~~~~~~~~~~~~~~~~~~~
+
+Will result in:
+
+~~~~~~~~~~~~~~~~~~~~
+# Added as an automation comment
+echo "Run me"
+~~~~~~~~~~~~~~~~~~~~
+
+And finally test commands like:
+
+~~~~~~~~~~~~~~~~~~~~
+<!--- AUTOMATION test=`echo "Testing foo"` -->
+~~~~~~~~~~~~~~~~~~~~
+
+Turns into:
+
+~~~~~~~~~~~~~~~~~~~~
+# Test codeblock
+echo "Testing foo"
+~~~~~~~~~~~~~~~~~~~~
+
+"""
+
 
 import re
 import sys
 
 import mistune
 
-
-"""
-This tool will parse <file> to extract enclosed ```bash ``` code blocks from markdown,
-add it to a shell script, and execute it (with -e -x set).
-* Usually when following documentation, some human interaction is required. This can be
-  achieved by adding:
-      <!-- AUTOMATION: execute=`cmd` -->
-  blocks to the markdown file.
-* Sometimes, some instructions enclosed in ```bash ``` may not be required and skipped.
-  This can be achieved by adding:
-      <!-- AUTOMATION: ignore=`reason` -->
-* Finally, testing that the instructions executed correctly can be tested using:
-      <!-- AUTOMATION: test=`cmd` -->
-"""
 
 # Create a code renderer
 class DocumentationCodeRenderer(mistune.AstRenderer):
@@ -33,7 +99,7 @@ class DocumentationCodeRenderer(mistune.AstRenderer):
 
     """
 
-    NAME = 'code-renderer'
+    NAME = "code-renderer"
     IS_TREE = True
 
     # If an ignore automation comment is given, ignore the next code block
@@ -66,7 +132,7 @@ class DocumentationCodeRenderer(mistune.AstRenderer):
         me = re.match(self.AUTOMATION_EXECUTE_REGEXP, children)
         if me:
             self.output_file.write("\n# Added as an automation comment\n")
-            self.output_file.write(str(me.group(1))+"\n\n")
+            self.output_file.write(str(me.group(1)) + "\n\n")
             return None
         #
         # Type: Ignore
@@ -82,20 +148,20 @@ class DocumentationCodeRenderer(mistune.AstRenderer):
         me = re.match(self.AUTOMATION_TEST_REGEXP, children)
         if me:
             self.output_file.write("\n# Test code block\n")
-            self.output_file.write(str(me.group(1))+"\n\n")
+            self.output_file.write(str(me.group(1)) + "\n\n")
             return None
         return None
 
     def block_code(self, children, info=None):
         """Only render a block of code if it is bash marked, and it is not ignored"""
         if info == "bash":
-          if self.IGNORE_NEXT_CODE_BLOCK:
-              self.IGNORE_NEXT_CODE_BLOCK = False
-              self.output_file.write("# Ignoring...\n")
-              for line in children.split("\n"):
-                  self.output_file.write("# {}\n".format(line))
-              return None
-          self.output_file.write(children+"\n\n")
+            if self.IGNORE_NEXT_CODE_BLOCK:
+                self.IGNORE_NEXT_CODE_BLOCK = False
+                self.output_file.write("# Ignoring...\n")
+                for line in children.split("\n"):
+                    self.output_file.write("# {}\n".format(line))
+                return None
+            self.output_file.write(children + "\n\n")
         return None
 
 
@@ -109,7 +175,7 @@ if __name__ == "__main__":
         data = af.read()
 
         try:
-          output_file = open(sys.argv[2], mode="w")
+            output_file = open(sys.argv[2], mode="w")
         except Exception as e:
             print("Error opening file: {}".format(e))
 
@@ -120,4 +186,3 @@ if __name__ == "__main__":
         markdown(data)
 
         output_file.close()
-
